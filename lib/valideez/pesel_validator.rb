6d6a8380661992592a67c5a5a5bb7d17fd84eb8f
zaiste <oh@zaiste.net>
2011-08-15 22:56:03 +0200

    passing options for Rails validator
    Signed-off-by: zaiste <oh@zaiste.net>

--- lib/valideez/pesel_validator.rb
+++ lib/valideez/pesel_validator.rb
@@ -3,8 +3,10 @@ require 'active_model'
 class PeselValidator < ActiveModel::EachValidator
 
   def validate_each(record, attribute, value)
-    message = options[:message] || "invalid format"
-    record.errors.add(attribute,  @message) unless Valideez::Pesel.new(value).valid?
+    @message = options[:message] || "invalid format"
+    options.delete(:message)
+
+    record.errors.add(attribute,  @message) unless Valideez::Pesel.new(value, options).valid?
   end
 end
 
