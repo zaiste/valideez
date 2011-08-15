8d15802adc93c2eba692ccd541809abddb64bae3
zaiste <oh@zaiste.net>
2011-08-15 22:42:18 +0200

    initialization fixed
    Signed-off-by: zaiste <oh@zaiste.net>

--- lib/valideez.rb
+++ lib/valideez.rb
@@ -1,4 +1,12 @@
-require "valideez/version"
+require 'valideez/version'
+
+require 'valideez/base'
+require 'valideez/common'
+require 'valideez/pesel'
+require 'valideez/nip'
+
+require 'valideez/pesel_validator'
+require 'valideez/nip_validator'
 
 module Valideez
   # Your code goes here...
