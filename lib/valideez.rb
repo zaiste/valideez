2029e25d9e94da920e45ec2b08d42802632189b2
zaiste <oh@zaiste.net>
2011-08-16 12:47:19 +0200

    initializatio file fixed
    Signed-off-by: zaiste <oh@zaiste.net>

--- lib/valideez.rb
+++ lib/valideez.rb
@@ -4,9 +4,11 @@ require 'valideez/base'
 require 'valideez/common'
 require 'valideez/pesel'
 require 'valideez/nip'
+require 'valideez/phone'
 
 require 'valideez/pesel_validator'
 require 'valideez/nip_validator'
+require 'valideez/phone_validator'
 
 module Valideez
   # Your code goes here...
